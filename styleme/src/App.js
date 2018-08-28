c9bc476bb82a231002049ffc80a469300ecc7afa
dhirajataws <dhirajataws@gmail.com>
2018-08-28 22:26:24 +1000

    next

--- styleme/src/App.js
+++ styleme/src/App.js
@@ -1,46 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import styled from 'styled-components';
 import tile from './tile.jpg';
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-`;
+import logo from './logo.png';
+
+
 const Img = styled.img`
 position: absolute;
     left: 0px;
     top: 0px;
-    z-index: -1;`
-    
+    `
+const ForgroundImg = styled.img`
+    position: absolute;
+        left: 0px;
+        top: 0px;
+        z-index: 1;
+        opacity:0.5;
+        `
 const ShowPicture = (props) => {
   return (
     <div>
-      <Img src={logo} />
+      <ForgroundImg src={logo} />
       {props.children}
     </div>
   )
 }
 
-const Myimage = (props) => <h1>jjjjj </h1>
+const ForImage = (props) => <img src={tile} />
+const addLogoName = (logo) => (Component) => <ShowPicture><Component /></ShowPicture>
 
-const addLogoName = (name) => {
-  return (Component) => {
-    return <ShowPicture name={name}>{Component}</ShowPicture>
-  }
-}
+
 const withLogo = addLogoName("logo");
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <Title>this</Title>
-        {withLogo(<Myimage />)}
+        </header> */}
+        {/* <ForImage /> */}
+        {withLogo(ForImage)}
       </div>
     );
   }
