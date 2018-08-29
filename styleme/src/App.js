027db1f12d809e54b75eb9e342af23ad2bf397cc
dhirajataws <dhirajataws@gmail.com>
2018-08-29 15:18:56 +1000

    next

--- styleme/src/App.js
+++ styleme/src/App.js
@@ -4,52 +4,6 @@ import styled from 'styled-components';
 import tile from './tile.jpg';
 import logo from './logo.png';
 
-const Row = styled.div`
-&::after{
-  content:",
-  clearboth;
-  display:table;
-}
-`;
-const Column = styled.div`
-float:left;
-${({ xs }) => (xs ? getWidthString(xs) : "width:100%")};
-
-@media only screen and (min-width:768px){
-  ${({ sm }) => sm && getWidthString(sm)};
-}
-@media only screen and (min-width:992px){
-  ${({ md }) => md && getWidthString(md)};
-}
-@media only screen and (min-width:1200px){
-  ${({ lg }) => lg && getWidthString(lg)};
-}
-`;
-function getWidthString(span) {
-  if (!span) return;
-  let width = span / 12 * 100;
-  return `width:${width}%;`;
-}
-export function Grid(props) {
-  return (
-    <div>
-      <Row>
-        <Column span="3">1</Column>
-        <Column span="1">2</Column>
-        <Column span="1">3</Column>
-        <Column span="1">4</Column>
-        <Column span="1">5</Column>
-        <Column span="1">6</Column>
-        <Column span="1">7</Column>
-        <Column span="1">8</Column>
-        <Column span="1">9</Column>
-        <Column span="1">10</Column>
-        <Column span="1">11</Column>
-        <Column span="1">12</Column>
-      </Row>
-    </div>
-  )
-}
 const Img = styled.img`
 position: absolute
     display:block;
@@ -71,7 +25,7 @@ const ShowPicture = (props) => {
   )
 }
 
-const ForImage = (props) => <img src={tile} />
+const ForImage = (props) => <Img src={tile} />
 const addLogoName = (logo) => (Component) => <ShowPicture><Component /></ShowPicture>
 
 
@@ -80,15 +34,27 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Row>
-          <Column xs="12" sm="12" md="12">
-            <Img src={tile} />
-          </Column>
-        </Row>
+        {withLogo(ForImage)}
       </div>
     );
   }
 }
+
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <Row>
+//           <Column sm="12" md="6" lg="6">
+//             <img src={tile} />
+//           </Column>
+
+//         </Row>
+//       </div>
+//     );
+//   }
+// }
 // class App extends Component {
 //   render() {
 //     return (
@@ -101,3 +67,54 @@ class App extends Component {
 
 
 export default App;
+
+
+
+// const Row = styled.div`
+// &::after{
+//   content:",
+//   clearboth;
+//   display:table;
+// }
+// `;
+// const Column = styled.div`
+// float:left;
+// ${({ xs }) => (xs ? getWidthString(xs) : "width:100%")};
+
+// @media only screen and (min-width:768px){
+//   ${({ sm }) => sm && getWidthString(sm)};
+// }
+// @media only screen and (min-width:992px){
+//   ${({ md }) => md && getWidthString(md)};
+// }
+// @media only screen and (min-width:1200px){
+//   ${({ lg }) => lg && getWidthString(lg)};
+// }
+// `;
+// function getWidthString(span) {
+//   if (!span) return;
+//   let width = span / 12 * 100;
+//   return `width:${width}%;`;
+// }
+
+
+// export function Grid(props) {
+//   return (
+//     <div>
+//       <Row>
+//         <Column span="3">1</Column>
+//         <Column span="1">2</Column>
+//         <Column span="1">3</Column>
+//         <Column span="1">4</Column>
+//         <Column span="1">5</Column>
+//         <Column span="1">6</Column>
+//         <Column span="1">7</Column>
+//         <Column span="1">8</Column>
+//         <Column span="1">9</Column>
+//         <Column span="1">10</Column>
+//         <Column span="1">11</Column>
+//         <Column span="1">12</Column>
+//       </Row>
+//     </div>
+//   )
+// }
